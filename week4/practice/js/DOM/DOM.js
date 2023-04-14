1102877b25930db6dd029fe669dfdb1ea54c6bbb
CJY <cjy3458@naver.com>
2023-04-14 21:12:36 +0900

    analyze code

--- week4/practice/js/DOM/DOM.js
+++ week4/practice/js/DOM/DOM.js
@@ -1,4 +1,5 @@
 class DOM {
+  /* tagName, innerText, className 매개변수를 받아 DOM 요소를 생성 */
   constructor(tagName, innerText, className) {
     this.node = document.createElement(tagName);
     this.node.innerText = innerText;
@@ -6,6 +7,17 @@ class DOM {
   }
 }
 
-//div, button, input(checkbox),
+/*
+
+DOM 클래스는 합니다.
+
+constructor 메서드 내부에서 document.createElement(tagName) 코드를 통해 새로운 DOM 요소를 생성합니다. 이 요소의 태그명은 tagName 매개변수로부터 전달받습니다.
+
+this.node.innerText = innerText 코드는 생성된 요소의 텍스트 내용을 innerText 매개변수로 지정합니다.
+
+if (className) this.node.classList.add(className) 코드는 className 매개변수가 존재할 경우, 생성된 요소의 클래스 리스트에 className을 추가합니다.
+
+
+*/
 
 export default DOM;
