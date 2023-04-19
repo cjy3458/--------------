9a0ea90fdc954cec9f0ae409b5fad1b324447b20
CJY <cjy3458@naver.com>
2023-04-19 19:47:21 +0900

    chore: fix error

--- week4/assignment/src/addCarName.js
+++ week4/assignment/src/addCarName.js
@@ -1,17 +1,26 @@
-export let newCar = "";
-
 const carForm = document.getElementById("car");
 const carInput = document.getElementById("car-names-input");
+class Car {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+export const carList = [];
 
 carForm.addEventListener("submit", (event) => {
   event.preventDefault();
-  newCar = carInput.value;
-  newCar = newCar.split(",").map((name) => name.trim());
-  newCar.forEach((n) => {
-    if (n.length > 5) {
+  const newCarNames = carInput.value.split(",").map((name) => name.trim());
+
+  newCarNames.forEach((name) => {
+    if (name.length > 5) {
       alert("5글자 이하로 입력하세요 -_-");
       carInput.value = "";
       return;
     }
+
+    const newCar = new Car(name);
+    console.log(newCar.name);
+    carList.push(newCar.name);
   });
 });
--- week4/assignment/src/countGame.js
+++ week4/assignment/src/countGame.js
@@ -1,7 +1,8 @@
-import { newCar } from "./addCarName.js";
+import { carList } from "./addCarName.js";
 import { addCarToList } from "./addCarToList.js";
 
 export function countGame(event) {
   event.preventDefault();
-  addCarToList(newCar, document.getElementById("racing-count-input").value);
+  addCarToList(carList, document.getElementById("racing-count-input").value);
+  document.getElementById("car-names-input").value = "";
 }
