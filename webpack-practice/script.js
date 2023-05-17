7e57ed9de0cb89d889ff01faaa4466da8f5c46af
CJY <cjy3458@naver.com>
2023-05-17 16:40:50 +0900

    week8assignment

--- webpack-practice/script.js
+++ webpack-practice/script.js
@@ -1,4 +1,17 @@
-var btn = document.getElementById("btn");
-btn.addEventListener("click", function () {
-  alert("Hello world");
-});
+// API 호출 예시 (async/await 사용)
+
+async function fetchAPI(endpoint) {
+  const response = await fetch(`/api${endpoint}`);
+  const data = await response.json();
+  return data;
+}
+
+// 사용 예시
+async function getData() {
+  try {
+    const result = await fetchAPI("/someEndpoint");
+    // 데이터 처리
+  } catch (error) {
+    // 오류 처리
+  }
+}
