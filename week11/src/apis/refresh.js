e6caa2da292d520f7f80bf9ada6c394308a7be2b
CJY <cjy3458@naver.com>
2023-07-08 21:02:43 +0900

    [FEAT] refresh 토큰 새로받기 기능 추가

---
+++ week11/src/apis/refresh.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const getNewRefresh = async () => {
+  const accessToken = localStorage.getItem("access");
+  const refreshToken = localStorage.getItem("refresh");
+
+  const result = await axios.post(
+    "http://front.cau-likelion.org/refresh",
+    {
+      refreshToken,
+    },
+    {
+      headers: {
+        Authorization: accessToken,
+      },
+    }
+  );
+
+  return result.data;
+};
--- week11/src/pages/Mypage.js
+++ week11/src/pages/Mypage.js
@@ -1,13 +1,25 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { getMyPage } from "../apis/mypage";
 
 const Mypage = () => {
+  const [data, setData] = useState();
+  const [loading, setLoading] = useState();
   useEffect(() => {
     getMyPage().then((a) => {
+      setData(a); // 데이터를 받아오는데 성공한다면
+      setLoading(false); // Loading 상태를 False로 변경
       console.log(a);
     });
   }, []);
-  return <div>Mypage</div>;
+
+  if (loading) return <div>로딩중입니다...!</div>; // loading이 true라면 로딩창을 띄워주기!
+
+  return (
+    <div>
+      <div>회원 성함: {data?.name}</div>
+      <div>회원 나이: {data?.age}</div>
+    </div>
+  );
 };
 
 export default Mypage;
