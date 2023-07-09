8b2e261ce36a3325805a0a13967742062c88abb4
CJY <cjy3458@naver.com>
2023-07-09 20:22:01 +0900

    [REAFACTOR] 코드 개선

--- week11/src/pages/Mypage.js
+++ week11/src/pages/Mypage.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { getMyPage } from "../apis/mypage";
+import { Wrapper3, Title, Information, Inputs } from "../components/Common";
 
 const Mypage = () => {
   const [data, setData] = useState();
@@ -8,17 +9,20 @@ const Mypage = () => {
     getMyPage().then((a) => {
       setData(a); // 데이터를 받아오는데 성공한다면
       setLoading(false); // Loading 상태를 False로 변경
-      console.log(a);
+      console.log(data);
     });
   }, []);
 
   if (loading) return <div>로딩중입니다...!</div>; // loading이 true라면 로딩창을 띄워주기!
 
   return (
-    <div>
-      <div>회원 성함: {data?.name}</div>
-      <div>회원 나이: {data?.age}</div>
-    </div>
+    <Wrapper3>
+      <Title>회원 정보</Title>
+      <Information>
+        <div>회원님 성함: {data?.name}</div>
+        <div>회원님 연세: {data?.age}세</div>
+      </Information>
+    </Wrapper3>
   );
 };
 
--- week11/src/pages/Signup.js
+++ week11/src/pages/Signup.js
@@ -1,18 +1,48 @@
 import React from "react";
-import { Title, Wrapper, Inputs, Input } from "../components/Common";
+import { Title, Inputs, Input, Wrapp, Wrapper2 } from "../components/Common";
+import { useForm } from "../hooks/useForm";
+import { styled } from "styled-components";
+import { signUp } from "../apis/signUp";
+import { useNavigate } from "react-router-dom";
 
 const Signup = () => {
+  const [id, onChangeId] = useForm();
+  const [pw, onChangePw] = useForm();
+  const [name, onChangeName] = useForm();
+  const [age, onChangeAge] = useForm();
+
+  const router = useNavigate();
+
+  const onClick = async () => {
+    await signUp(id, pw, name, age);
+    router("/");
+  };
+
   return (
-    <Wrapper>
-      <Title></Title>
+    <Wrapper2>
+      <Title>회원가입</Title>
       <Inputs>
-        <Input placeholder="아이디" />
-        <Input placeholder="비밀번호" type="password" />
-        <Input placeholder="성함" />
-        <Input placeholder="나이" />
+        <Input placeholder="아이디" value={id} onChange={onChangeId} />
+        <Input
+          placeholder="비밀번호"
+          type="password"
+          value={pw}
+          onChange={onChangePw}
+        />
+        <Input placeholder="성함" value={name} onChange={onChangeName} />
+        <Input placeholder="나이" value={age} onChange={onChangeAge} />
       </Inputs>
-    </Wrapper>
+      <Button onClick={onClick}>가입하기</Button>
+    </Wrapper2>
   );
 };
 
 export default Signup;
+
+const Button = styled.button`
+  background-color: black;
+  color: white;
+  padding: 10px 20px 10px 20px;
+  margin-top: 10%;
+  border-radius: 5px;
+`;
