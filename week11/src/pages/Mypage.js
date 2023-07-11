d704b80ed5c3aca53766f05ea2104e08cee6c942
CJY <cjy3458@naver.com>
2023-07-11 23:18:39 +0900

    [FEAT] 토큰 만료시 로그인 페이지로 이동

--- week11/src/pages/Mypage.js
+++ week11/src/pages/Mypage.js
@@ -1,16 +1,25 @@
 import React, { useEffect, useState } from "react";
 import { getMyPage } from "../apis/mypage";
-import { Wrapper3, Title, Information, Inputs } from "../components/Common";
+import { Wrapper3, Title, Information } from "../components/Common";
+import { useNavigate } from "react-router-dom";
 
 const Mypage = () => {
   const [data, setData] = useState();
   const [loading, setLoading] = useState();
+
+  const router = useNavigate();
+
   useEffect(() => {
-    getMyPage().then((a) => {
-      setData(a); // 데이터를 받아오는데 성공한다면
-      setLoading(false); // Loading 상태를 False로 변경
-      console.log(data);
-    });
+    getMyPage()
+      .then((a) => {
+        setData(a); // 데이터를 받아오는데 성공한다면
+        setLoading(false); // Loading 상태를 False로 변경
+        console.log(data);
+      })
+      .catch((error) => {
+        alert("로그인 시간 만료", error);
+        router("/");
+      });
   }, []);
 
   if (loading) return <div>로딩중입니다...!</div>; // loading이 true라면 로딩창을 띄워주기!
