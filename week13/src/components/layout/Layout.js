4f1ef8b1d9f3c9c230bdd61c693a3af7115ea2f8
CJY <cjy3458@naver.com>
2023-07-19 17:26:23 +0900

    [START] Project Setting

---
+++ week13/src/components/layout/Layout.js
@@ -0,0 +1,24 @@
+const Wrapper = styled.div`
+  margin: 0;
+  padding: 0;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Header = styled.div`
+  display: flex;
+  height: 100px;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+`;
+const Footer = styled.div`
+  display: flex;
+  height: 50px;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+`;
