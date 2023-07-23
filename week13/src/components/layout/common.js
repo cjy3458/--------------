91b4679e9a2960cf041ec9e3ee22fadd96bde3be
CJY <cjy3458@naver.com>
2023-07-23 17:48:28 +0900

    [FEAT] 회원가입기능 추가하기

--- week13/src/components/layout/common.js
+++ week13/src/components/layout/common.js
@@ -7,15 +7,82 @@ export const Wrapper = styled.div`
   align-items: center;
 `;
 
+export const BtnDiv = styled.div`
+  display: flex;
+  flex-wrap: nowrap;
+`;
+
 export const Button = styled.button`
   all: unset;
   background-color: ${(props) => props.mode};
   color: white;
   padding: 10px;
-  border-radius: 24px;
+  border-radius: 20px;
 `;
 
 export const Title = styled.div`
   font-size: 30px;
   margin: 20px;
+`;
+
+export const Input = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  input {
+    font-size: 15px;
+    color: #222222;
+    width: 300px;
+    border: none;
+    border-bottom: solid #aaaaaa 1px;
+    padding-bottom: 10px;
+    padding-left: 10px;
+    position: relative;
+    background: none;
+    z-index: 5;
+    margin-bottom: 10%;
+  }
+
+  input::placeholder {
+    color: #aaaaaa;
+  }
+  input:focus {
+    outline: none;
+  }
+
+  span {
+    display: block;
+    position: absolute;
+    bottom: 0;
+    left: 0%;
+    background-color: #666;
+    width: 0;
+    height: 2px;
+    border-radius: 2px;
+    transition: 0.5s;
+  }
+
+  label {
+    position: absolute;
+    color: #aaa;
+    left: 10px;
+    font-size: 20px;
+    bottom: 8px;
+    transition: all 0.2s;
+  }
+
+  input:focus ~ label,
+  input:valid ~ label {
+    font-size: 16px;
+    bottom: 40px;
+    color: #666;
+    font-weight: bold;
+  }
+
+  input:focus ~ span,
+  input:valid ~ span {
+    width: 100%;
+  }
 `;
