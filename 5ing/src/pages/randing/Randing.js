5f38157bf787a349e197e55f559fe04b6e992927
CJY <cjy3458@naver.com>
2023-08-04 01:03:07 +0900

    [FEAT] 하나하나 뜯어서 배치하기

--- 5ing/src/pages/randing/Randing.js
+++ 5ing/src/pages/randing/Randing.js
@@ -5,11 +5,15 @@ import Randing1 from "./Randing1";
 
 const Randing = () => {
   return (
-    <>
+    <Wrapper>
       <Headers></Headers>
       <Randing1></Randing1>
-    </>
+    </Wrapper>
   );
 };
 
 export default Randing;
+
+const Wrapper = styled.div`
+  width: 100%;
+`;
--- 5ing/src/pages/randing/Randing1.js
+++ 5ing/src/pages/randing/Randing1.js
@@ -1,14 +1,66 @@
 import React from "react";
 import { styled } from "styled-components";
+import halfduck from "../../img/halfduck.png";
+import cloud1 from "../../img/cloud1.png";
+import cloud2 from "../../img/cloud2.png";
+import logo from "../../img/logo.png";
 
 const Randing1 = () => {
-  return <Wrapper>Randing1</Wrapper>;
+  return (
+    <Wrapper>
+      <Rand1>
+        <Image1 src={cloud2} />
+        <Image2 src={halfduck} />
+        <Image3 src={cloud1} />
+        <Image4 src={logo} />
+      </Rand1>
+    </Wrapper>
+  );
 };
 
 export default Randing1;
 
 const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
   width: 100%;
-  height: 799.957px;
+  height: 800px;
   background-color: #89cdf6;
+`;
+
+const Rand1 = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+const Image1 = styled.img`
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  height: 50%;
+  margin-bottom: 2%;
+`;
+
+const Image2 = styled.img`
+  position: absolute;
+  right: 0;
+  margin-right: 11%;
+  margin-top: 6%;
+  height: 70%;
+`;
+
+const Image3 = styled.img`
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+`;
+
+const Image4 = styled.img`
+  position: absolute;
+  top: 0;
+  margin-top: 13%;
+  width: 30%;
+  margin-left: 10%;
 `;
