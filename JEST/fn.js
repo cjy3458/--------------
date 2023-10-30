c93a4fd3a38ca74f7b71e4b0d24b5437a3b8411d
CJY <cjy3458@naver.com>
2023-10-30 18:02:22 +0900

    test

--- JEST/fn.js
+++ JEST/fn.js
@@ -3,6 +3,7 @@
 const fn = {
   add: (num1, num2) => num1 + num2,
   makeUser: (name, age) => ({ name, age }),
+  student: (name, age) => ({ name, age, gender: undefined }),
 };
 
 module.exports = fn;
--- JEST/fn.test.js
+++ JEST/fn.test.js
@@ -1,33 +1,38 @@
-// 아까 만든 함수 불러오기
+// // 아까 만든 함수 불러오기
 const fn = require("./fn");
 
-// 테스트에 대한 설명 친저하게
-test("1은 1이야", () => {
-  expect(1).toBe(1); // expect 검증값, toBe 기대값
+// // 테스트에 대한 설명 친저하게
+// test("1은 1이야", () => {
+//   expect(1).toBe(1); // expect 검증값, toBe 기대값
+// });
+
+// test("2+3은 5야", () => {
+//   expect(fn.add(2, 3)).toEqual(5); // Matcher
+// });
+
+// test("3+3은 5야", () => {
+//   expect(fn.add(3, 3)).toBe(5);
+// });
+
+test("이름과 나이를 전달받아서 객체 반환", () => {
+  expect(fn.makeUser("Jay", 30)).toBe({
+    name: "Jay",
+    age: 30,
+  });
 });
 
-test("2+3은 5야", () => {
-  expect(fn.add(2, 3)).toBe(5); // Matcher
-});
+//객체는 재귀적으로 값을 확인해야 하기 때문에 toEqual을 사용
 
-test("3+3은 5야", () => {
-  expect(fn.add(3, 3)).toBe(5);
+test("이름과 나이를 전달받아서 객체 반환", () => {
+  expect(fn.makeUser("Jay", 30)).toEqual({
+    name: "Jay",
+    age: 30,
+  });
 });
 
 test("이름과 나이를 전달받아서 객체 반환", () => {
-  expect(
-    fn.makeUser("Jay", 30).toBe({
-      name: "Jay",
-      age: 30,
-    })
-  );
-});
-
-test("이름과 나이를 전달받아서 객체 반환", () => {
-  expect(
-    fn.makeUser("Jay", 30).toStrictEqual({
-      name: "Jay",
-      age: 30,
-    })
-  );
+  expect(fn.makeUser("Jay", 30)).toStrictEqual({
+    name: "Jay",
+    age: 30,
+  });
 });
